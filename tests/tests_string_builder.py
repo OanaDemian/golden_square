72dbc21c8950d17981b050f1c6c8467dd16a2c4e
Oana Demian <oana.demian@gmail.com>
2025-10-06 16:13:16 +0100

    tests the behaviour of StringBuilder when more strings are added

--- tests/tests_string_builder.py
+++ tests/tests_string_builder.py
@@ -1,11 +1,11 @@
 from lib.string_builder import *
 
 """
-If the added string is the single word 'Oana',
-Returns "Oana" and 4 when .output(), .size() are called on the StringBuilder instance 
+When we add a single word,
+Returns the word and word's length when .output(), .size() are called on the StringBuilder instance 
 """
 
-def test_string_builder():
+def test_string_builder_with_single_word():
     string_builder = StringBuilder()
     string_builder.add("Oana")
     actual_string = string_builder.output()
@@ -17,27 +17,11 @@ def test_string_builder():
 
 
 """
-If the added string is the sentence 'Oana write some tests!',
-Returns Oana write some tests!' and 22 when .output(), .size() are called on the StringBuilder instance 
-"""
-
-def test_string_builder():
-    string_builder = StringBuilder()
-    string_builder.add("Oana")
-    actual_string = string_builder.output()
-    expected_string = "Oana write some tests!"
-    actual_string_length = string_builder.size()
-    expected_string_length = 22
-    assert actual_string == expected_string 
-    assert actual_string_length == expected_string_length
-
-
-"""
-If the added string is an empty string',
+When we add an empty string',
 Returns '' and 0 when .output(), .size() are called on the StringBuilder instance 
 """
 
-def test_string_builder():
+def test_string_builder_with_empty_string():
     string_builder = StringBuilder()
     string_builder.add("")
     actual_string = string_builder.output()
@@ -46,3 +30,32 @@ def test_string_builder():
     expected_string_length = 0
     assert actual_string == expected_string 
     assert actual_string_length == expected_string_length
+
+"""
+When we add multiple strings',
+The output is those string concatenated 
+"""
+
+def test_string_builder_with_multiple_strings():
+    string_builder = StringBuilder()
+    string_builder.add("Oana")
+    string_builder.add("add")
+    string_builder.add("more")
+    string_builder.add("tests!")
+    actual_string = string_builder.output()
+    expected_string = "Oana add more tests!"
+    assert actual_string == expected_string 
+"""
+When we add multiple strings',
+The size is the sizw of all those strings added 
+"""
+
+def test_string_builder_size_when_multiple_strings_added_successively():
+    string_builder = StringBuilder()
+    string_builder.add("Oana")
+    string_builder.add("add")
+    string_builder.add("more")
+    string_builder.add("tests!")
+    actual_string_length = string_builder.size()
+    expected_string_length = 21
+    assert actual_string_length == expected_string_length 
