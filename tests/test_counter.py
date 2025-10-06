ecafc4f151c661e2d078323e7e8c3ae7b03b1438
Oana Demian <oana.demian@gmail.com>
2025-10-06 15:42:45 +0100

    fixes the names of methods in test_counter file

--- tests/test_counter.py
+++ tests/test_counter.py
@@ -5,7 +5,7 @@ if the number is negative -5,
 returns  "Counted to -5 so far."
 """
 
-def test_counter():
+def test_counter_negative():
     counter = Counter()
     counter.add(-5)
     actual = counter.report()
@@ -16,7 +16,7 @@ if the number is positive 5,
 returns  "Counted to 5 so far."
 """
 
-def test_counter():
+def test_counter_positive():
     counter = Counter()
     counter.add(5)
     actual = counter.report()
@@ -28,7 +28,7 @@ if the number is 0,
 returns  "Counted to 0 so far."
 """
 
-def test_counter():
+def test_counter_zero():
     counter = Counter()
     counter.add(0)
     actual = counter.report()
@@ -41,7 +41,7 @@ if the number is a float 9.17,
 returns  "Counted to 9.17 so far."
 """
 
-def test_counter():
+def test_counter_float():
     counter = Counter()
     counter.add(0)
     actual = counter.report()
